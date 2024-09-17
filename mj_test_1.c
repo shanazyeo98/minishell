0310245f98c763d839aca1f59b82ac1e2911cf9a
MJ <mingjuntan93@gmail.com>
2024-09-18 06:04:28 +0800

    closed ;

--- mj_test_1.c
+++ mj_test_1.c
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 04:40:40 by mintan            #+#    #+#             */
-/*   Updated: 2024/09/18 06:03:56 by mintan           ###   ########.fr       */
+/*   Updated: 2024/09/18 06:04:17 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	main(void)
 {
 	printf("This is a test\n");
 	printf("Test git commit --amend\n");
-	printf("Try creating branch from vs code")
+	printf("Try creating branch from vs code");
 }
