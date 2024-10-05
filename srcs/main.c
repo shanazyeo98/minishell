85c36f9e4371668206e9a4df31c0fb851e8e2397
MJ 42 <mingjuntan93@gmail.com>
2024-10-05 17:18:31 +0800

    Mj tests for tokenizing

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:11:07 by mintan            #+#    #+#             */
-/*   Updated: 2024/10/05 09:56:08 by mintan           ###   ########.fr       */
+/*   Updated: 2024/10/05 13:34:12 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,17 @@ int	main(void)
 	t_minishell	ms;
 
 	ms = init_ms();
+
+
 	init_all_sig_handler();
 	while (1)			//use a global variable to store the signal for the while loop?
 	{
-
 		getinput(&ms);
-		if (ms.input == NULL || (ft_strncmp(ms.input, EXIT_CMD, 4) == 0 &&
-		ft_strlen(ms.input) == 4))
+		if (ms.input == NULL || ft_strcmp(ms.input, EXIT_CMD) == 0)
 			break_shell(&ms);
 		tokenize(ms.input, &ms);
+		print_token_list(ms);
+
 	}
 	return (EXIT_SUCCESS);
 	//Logic
