601ee524d352bbdf4324941a27f2171dbcb1c684
Shanaz <153274702+shanazyeo98@users.noreply.github.com>
2024-10-07 13:42:03 +0800

    parsing tree - to kiv

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: shayeo <shayeo@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 15:28:46 by shayeo            #+#    #+#             */
-/*   Updated: 2024/10/06 23:06:00 by shayeo           ###   ########.fr       */
+/*   Updated: 2024/10/07 12:16:48 by shayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,26 @@ int	ret_grp(t_token *token, int op)
 	return (grp);
 }
 
+void	assignbranch(t_ast **branch, t_ast **newbranch)
+{
+	int		id;
+	t_ast	*node;
+
+	if (*branch == NULL)
+		*branch = *newbranch;
+	else
+	{
+		id = (*newbranch)->id;
+		node = *branch;
+		while (node != NULL && node->type == OP)
+		{
+			if (node->id < id)
+			node->right = *newbranch;
+			node = node->left;
+		}
+	}
+}
+
 void	parse(t_minishell *params)
 {
 	int	grp_min;
@@ -56,8 +76,7 @@ void	parse(t_minishell *params)
 	while (grp_min <= grp_max)
 	{
 		branch = createbranch(params, grp_min);
-		if (*(params->ast) == NULL)
-			*(params->ast) = *branch;
+		assignbranch(params->ast, branch);
 		free(branch);
 		grp_min++;
 	}
