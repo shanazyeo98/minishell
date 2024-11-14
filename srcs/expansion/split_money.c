309307fb86ebd8d13037118b36bb116ba1c4a1cd
MJ <mingjuntan93@gmail.com>
2024-11-14 14:29:41 +0800

    push to pull

--- srcs/expansion/split_money.c
+++ srcs/expansion/split_money.c
@@ -6,108 +6,22 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 09:05:33 by mintan            #+#    #+#             */
-/*   Updated: 2024/11/14 13:02:14 by mintan           ###   ########.fr       */
+/*   Updated: 2024/11/14 13:31:44 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 
-/* Description: counts the number of words after the word is separated
-   by the delimiter. Counts the number of transitions from delimiter
-   to non-delimiter. */
-
-static int	countwrds(char const *s, char c)
-{
-	int	count;
-	int	i;
-
-	count = 0;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (i == 0 && s[i] != c)
-			count++;
-		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-			count++;
-		i++;
-	}
-	return (count);
-}
-
-/* Description: Moves the str pointer to the start of a character after the
-delimiter. Stops before the '\0'. */
-
-static const char	*findwrdstrt(char const *s, char c)
-{
-	while (*s == c && *s != '\0')
-		s++;
-	return (s);
-}
-
-/* Description: Counts the len of the word based on the current position of
-   the string. Checks when a delimiter or '\0' is reached. */
-
-static size_t	findwrdlen(char const *s, char c)
-{
-	size_t	count;
-	int		i;
-
-	count = 1;
-	i = 0;
-	while (s[i + 1] != c && s[i + 1] != '\0')
-	{
-		i++;
-		count++;
-	}
-	return (count);
-}
 
 
-/* Description: Allocates with malloc(3) and returns an array of strings
-   obtained by spliting 's' using the character 'c' as a delimiter. The
-   array must end with a NULL pointer. */
 
-char	**ft_split(char const *s, char c)
-{
-	char	**ret;
-	int		numwrd;
-	int		i;
-
-	i = 0;
-	numwrd = countwrds(s, c);
-	ret = (char **)malloc((numwrd + 1) * sizeof(char *));
-	if (ret == NULL)
-		return (NULL);
-	while (i < numwrd)
-	{
-		s = findwrdstrt(s, c);
-		ret[i] = ft_substr(s, 0, findwrdlen(s, c));
-		if (ret[i] == NULL)
-		{
-			freeall(ret, i);
-			free(ret);
-			return (NULL);
-		}
-		s = s + findwrdlen(s, c);
-		i++;
-	}
-	ret[i] = NULL;
-	return (ret);
-}
-		//find start and len of word -> done
-			//fx to move str to start of word -> done
-			//fx to count len of word -> done
-		//ft_substring of word into char* -> done
-			//if char* == NULL, fx to free all and break out of loop
-			//free the big malloc
-		//move the word start to end of the word -> done
 
 
 /* Description: given a string
 */
 
-t_list	*init_sploney(t_list **split, int start, int end, char *input)
+t_list	*get_split(int start, int end, char *input)
 {
 	t_list	*node;
 	char	*content;
@@ -140,13 +54,15 @@ t_list	*init_sploney(t_list **split, int start, int end, char *input)
 	generate a linked list of contents that will undergo expansion
 */
 
-t_list	*split_money(char *str)
+t_list	**split_money(char *str)
 {
 	int		strt_idx;
 	int		end_idx;
-	t_list	*split;
+	t_list	**split;
+	t_list	*node;
 
 	strt_idx = 0;
+	split = NULL;
 	while (str[strt_idx] != '\0')
 	{
 		if (str[strt_idx] == '$' && \
@@ -160,8 +76,31 @@ t_list	*split_money(char *str)
 			while ((str[end_idx + 1] != '$') || (str[end_idx + 1] != '\0'))
 				end_idx++;
 		}
+		node = get_split(strt_idx, end_idx, str);
+		//error handling here
+		ft_lstadd_back(split, node);
 		//start -> strt_idx
 		//end -> end_idx
 		strt_idx = end_idx + 1;
 	}
+	return (split);
+}
+
+
+
+int	main(void)
+{
+	char	*in1;
+	char	*in2;
+	char	*in3;
+	char	*in4;
+
+	in1 = ft_strdup("$");
+	in2 = ft_strdup("$");
+
+
+
+
+
+
 }
