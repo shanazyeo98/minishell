cc41ea3476a15957864fab29a4ce5a049454b205
Shanaz <153274702+shanazyeo98@users.noreply.github.com>
2024-11-16 16:08:44 +0800

    fixed issues where there is a seg fault if there is multiple redirections

--- srcs/updatetree/updatetree_fill.c
+++ srcs/updatetree/updatetree_fill.c
@@ -6,7 +6,7 @@
 /*   By: shayeo <shayeo@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 02:44:30 by shayeo            #+#    #+#             */
-/*   Updated: 2024/11/12 14:49:00 by shayeo           ###   ########.fr       */
+/*   Updated: 2024/11/16 16:01:33 by shayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ int	fill(t_cmd *cmd, t_token *start, t_token *end)
 		start = start->next;
 		if (start != NULL && start->wordgrp != grp)
 		{
-			if (cmd->args[i_arg] != NULL)
+			if (cmd->args != NULL && cmd->args[i_arg] != NULL)
 				i_arg++;
 			grp = start->wordgrp;
 		}
